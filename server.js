c497f76590129de91e898e734406b7986f96046c
petejos80 <petejos80@gmail.com>
2018-05-27 17:19:27 -0500

    Changed code in server.js

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 var passport = require('passport')
 var session = require('express-session')
 var bodyParser = require('body-parser')
-var env = require('dotenv').load();
-var models = require("./models");
+var env = require('dotenv').load()
 var exphbs = require('express-handlebars')
-var authRoute = require('./app/routes/auth.js')(app);
-var authRoute = require('./app/routes/auth.js')(app,passport);
-
  
-//Sync Database
-models.sequelize.sync().then(function() {
- 
-    console.log('Nice! Database looks fine')
- 
-}).catch(function(err) {
- 
-    console.log(err, "Something went wrong with the Database Update!")
- 
-});
-
-//load passport strategies
-require('./config/passport/passport.js')(passport, models.user);
-
-// Use bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
+// For BodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
-
-// Use Passport
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // Code for session secret
+ 
+ 
+// For Passport
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
-app.use(passport.session()); // Code for persistent login sessions
-
-// Use Handlebars
+app.use(passport.session()); // persistent login sessions
+ 
+ 
+// For Handlebars
 app.set('views', './app/views')
 app.engine('hbs', exphbs({
     extname: '.hbs'
@@ -41,9 +31,33 @@ app.engine('hbs', exphbs({
 app.set('view engine', '.hbs');
  
  
+ 
 app.get('/', function(req, res) {
  
-    res.send('Welcome to the login page');
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+//Models
+var models = require("./models");
+ 
+//Routes
+var authRoute = require('./app/routes/auth.js')(app,passport);
+ 
+ 
+//Load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+ 
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
  
 });
  
@@ -51,7 +65,9 @@ app.get('/', function(req, res) {
 app.listen(5000, function(err) {
  
     if (!err)
-        console.log("Login is online");
+ 
+        console.log("Site is live");
+         
     else console.log(err)
  
-});
+}); 
