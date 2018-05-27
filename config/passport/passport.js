071aaa6842c6e453c7301e2e1f39871c80f6f3cc
petejos80 <petejos80@gmail.com>
2018-05-27 18:26:31 -0500

    Working draft of passport, not yet integrated with app

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -11,6 +11,7 @@ module.exports = function(passport, user) {
  
  
     passport.use('local-signup', new LocalStrategy(
+
  
         {
  
@@ -21,6 +22,8 @@ module.exports = function(passport, user) {
             passReqToCallback: true // allows us to pass back the entire request to the callback
  
         },
+
+        
  
  
  
@@ -31,8 +34,33 @@ module.exports = function(passport, user) {
                 return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
  
             };
- 
- 
+
+        
+        // Serialize
+        passport.serializeUser(function(user, done) {
+        
+            done(null, user.id);
+        
+        });
+
+        // Deserialize user 
+        passport.deserializeUser(function(id, done) {
+        
+            User.findById(id).then(function(user) {
+        
+                if (user) {
+        
+                    done(null, user.get());
+        
+                } else {
+        
+                    done(user.errors, null);
+        
+                }
+        
+            });
+        
+        });
  
             User.findOne({
                 where: {
@@ -90,5 +118,73 @@ module.exports = function(passport, user) {
         }
  
     ));
+
+    //LOCAL SIGNIN
+passport.use('local-signin', new LocalStrategy(
+ 
+    {
+ 
+        // by default, local strategy uses username and password, we will override with email
+ 
+        usernameField: 'email',
+ 
+        passwordField: 'password',
+ 
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+ 
+    },
+ 
+ 
+    function(req, email, password, done) {
+ 
+        var User = user;
+ 
+        var isValidPassword = function(userpass, password) {
+ 
+            return bCrypt.compareSync(password, userpass);
+ 
+        }
+ 
+        User.findOne({
+            where: {
+                email: email
+            }
+        }).then(function(user) {
+ 
+            if (!user) {
+ 
+                return done(null, false, {
+                    message: 'Email does not exist'
+                });
+ 
+            }
+ 
+            if (!isValidPassword(user.password, password)) {
+ 
+                return done(null, false, {
+                    message: 'Incorrect password.'
+                });
+ 
+            }
+ 
+ 
+            var userinfo = user.get();
+            return done(null, userinfo);
+ 
+ 
+        }).catch(function(err) {
+ 
+            console.log("Error:", err);
+ 
+            return done(null, false, {
+                message: 'Something went wrong with your Signin'
+            });
+ 
+        });
+ 
+ 
+    }
+ 
+));
  
 }
