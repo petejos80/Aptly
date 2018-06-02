4ba964603616e7ab28d6f59b6624a42f024996b0
unknown <bubbastinx@gmail.com>
2018-06-01 23:21:48 -0600

    current routes in api-routes are operational further expansion and tweaking still need to be done

--- models/user.js
+++ models/user.js
@@ -4,107 +4,102 @@ module.exports = function (sequelize, DataTypes) {
 
     let User = sequelize.define("User", {
         userId:
-            {
-                type: DataTypes.BIGINT,
-                allowNull: true,
-                primaryKey: true, 
-                autoIncrement: true, 
-                validation: {
-                    is: /^[a-z]+$/i 
-                }         
-            },  
-        userName:
-            {
-                type:  DataTypes.STRING, 
-                allowNull: false,
-                validation:  {
-                    is: /^[a-z]+$/i       
-                }
-            },
-        userPsswd:
-            {
-                type: DataTypes.STRING,
-                allowNull: false
-            }, 
-        firstName:
-            {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validation:  {
-                    is: /^[a-z]+$/i         
-                } 
-            }, 
-        lastName:
-            {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validation: {
-                    is: /^[a-z]+$/i        
-                } 
-            } ,
-        unitNumber:
-            {
-                type: DataTypes.STRING,
-                allowNull: false, 
-                validation: {
-                    is: /^[a-z]+$/i          
-                }
-            }, 
-        email:
-            {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validation: {
-                    isEmail: true, 
-                    is: /^[a-z]+$/i        
-                },        
-            }, 
-        leaseEnd:
-            {
-                type: DataTypes.STRING,
-                allowNull: true, 
-                validation:{
-                    isDate: true
-                } 
-            },  
-        phone:
-            {
-                type: DataTypes.STRING,
-                allowNull: true,
-                defaultValue: DataTypes.UUIDV1, 
-                validation: {
-                    isNumeric: true        
-                }
-            }, 
-        interest:
-            {
-                type:  DataTypes.STRING,
-                allowNull: true, 
-                validation: {
-                    notEmpty: true
-                }
-            }, 
-        address:
-            {
-                type: DataTypes.STRING,
-                allowNull: true, 
-                validation: {
-                    is: /^[a-z]+$/i         
-                }
-            },  
-        geoLocation:
-            {
-                type: DataTypes.STRING,
-                allowNull: true,
-                validation: {
-                    isDecimal: true
-                } 
-            }, 
-        cookies:
-            {
-                type: DataTypes.STRING,
-                allowNull: true
+         {
+            type: DataTypes.BIGINT,
+            allowNull: true,
+            primaryKey: true,
+            autoIncrement: true,
+            validation: {
+                is: /^[a-z]+$/i
             }
+        },
+        userName: 
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                is: /^[a-z]+$/i
+            }
+        },
+        userPsswd: 
+        {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: 
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                is: /^[a-z]+$/i
+            }
+        },
+        lastName: 
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                is: /^[a-z]+$/i
+            }
+        },
+        unitNumber: 
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                is: /^[a-z]+$/i
+            }
+        },
+        email: 
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                isEmail: true,
+                is: /^[a-z]+$/i
+            },
+        },
+        leaseEnd: 
+        {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validation: {
+                isDate: true
+            }
+        },
+        phone: 
+        {
+            type: DataTypes.STRING,
+            allowNull: true,
+            defaultValue: DataTypes.UUIDV1,
+            validation: {
+                isNumeric: true
+            }
+        },
+        interest: 
+        {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validation: {
+                notEmpty: true
+            }
+        },
+        address: 
+        {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validation: {
+                is: /^[a-z]+$/i
+            }
+        },
+        geoLocation: 
+        {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validation: {
+                isDecimal: true
+            }
+        }
     });
     return User
 }
